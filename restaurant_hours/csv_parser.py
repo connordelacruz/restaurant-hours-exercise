a03a810e5103a642923a66bc92c9e832eca479b4
Connor de la Cruz <connor.c.delacruz@gmail.com>
2025-12-12 12:18:01 -0600

    read csv file (hours parsing not yet implemented)

--- restaurant_hours/csv_parser.py
+++ restaurant_hours/csv_parser.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import csv
+import pprint
 
 # ================================================================================
 # Main (For Testing)
@@ -6,7 +8,18 @@
 
 def main():
     '''Function to call when file is executed directly.'''
-    print('Hello World!')
+    restaurant_data = {}
+    with open('restaurants.csv') as file:
+        reader = csv.reader(file)
+        # Skip header row
+        next(reader)
+        for row in reader:
+            name = row[0]
+            hours_string = row[1]
+            # TODO: parse hours_string
+            restaurant_data[name] = hours_string
+    pprint.pp(restaurant_data)
+
 
 if __name__ == '__main__':
     main()
