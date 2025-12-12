7f0961b3552ee6de8c7c78beb03e5f46a109b76f
Connor de la Cruz <connor.c.delacruz@gmail.com>
2025-12-12 12:08:27 -0600

    skeleton for csv parser

---
+++ restaurant_hours/csv_parser.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# ================================================================================
+# Main (For Testing)
+# ================================================================================
+
+def main():
+    '''Function to call when file is executed directly.'''
+    print('Hello World!')
+
+if __name__ == '__main__':
+    main()
